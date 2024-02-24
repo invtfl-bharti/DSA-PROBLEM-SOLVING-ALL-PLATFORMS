78d78ddec22923172336dfa0f6207f9bb79e2562
Bharti Jayprakash <154729771+invtfl-bharti@users.noreply.github.com>
2024-02-24 22:35:04 +0530

    Create 24-02-24-find-the-highest-number.cpp

---
+++ Geeks-for-Geeks/24-02-24-find-the-highest-number/24-02-24-find-the-highest-number.cpp
@@ -0,0 +1,54 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution {
+public:
+    int findPeakElement(vector<int>& a) 
+    {
+        // Code here.
+        
+        int left = 0;
+        int right = a.size()-1;
+        int max = a[0];
+        
+        while(left<=right) {
+            int mid = left+(right-left)/2;
+            if(a[mid]>a[mid-1] && a[mid]>a[mid+1]){
+                max = a[mid];
+            }
+            if(a[mid]<a[mid+1]) {
+                left = mid+1;
+            }
+            else{
+                right = mid-1;
+            }
+        }
+        return max;
+        
+    }
+};
+
+
+
+//{ Driver Code Starts.
+int main(){
+    int T;
+    cin >> T;
+    while(T--)
+    {
+    	int n;
+    	cin >> n;
+    	vector<int>a(n);
+    	for(int i = 0; i < n; i++)
+    		cin>>a[i];
+    	Solution ob;
+    	int ans = ob.findPeakElement(a);
+    	cout << ans << "\n";
+    }
+	return 0;
+}
+
+
+// } Driver Code Ends
