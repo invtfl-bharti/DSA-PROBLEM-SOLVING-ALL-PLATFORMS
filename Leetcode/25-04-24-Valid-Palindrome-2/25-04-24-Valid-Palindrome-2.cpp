d2f1765fd312e7279335239e0f838cb117180724
Bharti Jayprakash <154729771+invtfl-bharti@users.noreply.github.com>
2024-04-25 22:43:42 +0530

    Create 25-04-24-Valid-Palindrome-2.cpp

---
+++ Leetcode/25-04-24-Valid-Palindrome-2/25-04-24-Valid-Palindrome-2.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+   bool checkPalindrome(string s,int i,int j) {
+       while(i <= j) {
+            if(s[i]!=s[j]) {
+                return false;
+           }
+            else{
+                i++;
+                j--;
+           }
+       }
+       return true;
+   }
+    bool validPalindrome(string s) {
+         int i = 0;
+         int j = s.length()-1;
+         while(i < j) {
+            if(s[i]==s[j]) {
+                i++;
+                j--;
+            }
+            else{
+                // Removing ith character
+                bool ans1 = checkPalindrome(s,i+1,j);
+                bool ans2 = checkPalindrome(s,i,j-1);
+                return ans1 || ans2;
+            }
+         }
+        //  when there is no removal
+         return true;
+    }
+};
