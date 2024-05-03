7134a55d28fe15589c9c9473f7a111ccfee793ec
Bharti Jayprakash <154729771+invtfl-bharti@users.noreply.github.com>
2024-05-04 01:22:34 +0530

    Create 3-05-24-Goal-Parser-Interpretation.cpp

---
+++ Leetcode/3-05-24-Goal-Parser-Interpretation/3-05-24-Goal-Parser-Interpretation.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    string interpret(string command) {
+        string ans = "";
+        for(int i=0;i<command.size();i++) {
+            if(command[i]=='G') {
+                ans+='G';
+            }
+            if(command[i]=='(' && command[i+1]==')') {
+                    ans+='o';
+            }
+            if(command[i]=='(' && command[i+1]=='a') {
+                ans+='a';
+                ans+='l';
+            }
+        }
+        return ans;
+    }
+};
