057e8187801174c599ad18effdec475e33a2f43a
Bharti Jayprakash <154729771+invtfl-bharti@users.noreply.github.com>
2024-10-30 22:46:55 +0530

    Create 30-10-24-Pairs with difference k.cpp

---
+++ Geeks-for-Geeks/30-10-24-Pairs with difference k/30-10-24-Pairs with difference k.cpp
@@ -0,0 +1,54 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+// } Driver Code Ends
+// User function template for C++
+class Solution {
+  public:
+    /* Returns count of pairs with difference k  */
+    int countPairsWithDiffK(vector<int>& arr, int k) {
+        // code here
+        int ans = 0;
+        unordered_map<int,int>freqMap;
+        for(int i = 0; i < arr.size(); i++) {
+            freqMap[arr[i]]++;
+        }
+        for(int i = 0; i < arr.size(); i++) {
+            if(freqMap.find(arr[i] + k) != freqMap.end()) {
+                ans += freqMap[arr[i] + k];
+            }
+        }
+        return ans;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    string ts;
+    getline(cin, ts);
+    int t = stoi(ts);
+    while (t--) {
+
+        vector<int> arr;
+        string input;
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+        string ks;
+        getline(cin, ks);
+        int k = stoi(ks);
+        Solution ob;
+        auto ans = ob.countPairsWithDiffK(arr, k);
+        cout << ans << "\n";
+        cout << "~" << endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
